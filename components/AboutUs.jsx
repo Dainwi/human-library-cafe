9150b91b5170a1851617aa67c465113868f48a61
Dainwi Kumar <danwichoudhary@gmail.com>
2023-09-19 19:17:11 +0530

    .

--- components/AboutUs.jsx
+++ components/AboutUs.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import aboutData from '@/data/AboutUsData'
 
 export default function AboutUs() {
   return (
     <section className='about-us'>
       <h1>Welcome to the Human Library Cafe</h1>
       <p> Learn - Connect - Share</p>
+      {aboutData.AboutData.map((item, index) => {
+        <div className="about-card">
+          <h3>{item.title}</h3>
+          <p>
+            {item.content}
+          </p>
+        </div>
+      })}
     </section>
   )
 }
---
+++ data/AboutUsData.js
@@ -0,0 +1,18 @@
+const aboutData = {
+  AboutData: [
+    {
+      title: 'The Concept',
+      content: ' The Human Library Cafe is a community of readers who share their experiences and thoughts with each other. '
+    },
+    {
+      title: 'Our Books',
+      content: 'All of our human books are volunteers with personal experience with their topic. The Human Library® is a place where difficult questions are expected, appreciated and answered.'
+    },
+    {
+      title: 'Our Mission',
+      content: 'Our mission is to create a safe space for dialogue where topics are discussed openly between our human books and their readers. '
+    }
+  ],
+
+}
+export default aboutData;
