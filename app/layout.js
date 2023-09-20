8c465e284cf2a12a14f5afce0f9c47c7260378e7
Dainwi Kumar <danwichoudhary@gmail.com>
2023-09-20 23:15:54 +0530

    .

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,5 @@
-// import Header from '@/components/Header'
 import Header from '../components/Header'
-import Footer from '../components/Footer'
 import './globals.css'
-// import { Inter } from 'next/font/google'
-
-// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -20,7 +15,32 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
-        <Footer/>
+        {/* <Footer/> */}
+        <footer className="footer">
+          <div className="footer-content">
+            <div className="footer-logo">
+              <h2>Human Library Cafe</h2>
+            </div>
+            <div className="footer-links">
+              <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+              </ul>
+            </div>
+            <div className="footer-social">
+              <ul>
+                <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+                <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="footer-bottom">
+            &copy; {new Date().getFullYear()} Human Library Cafe. All rights reserved.
+          </div>
+        </footer>
       </body>
     </html>
   )
