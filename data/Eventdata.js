0e72714fcb2a62cd41e481b312dd27271a07610d
Dainwi Kumar <danwichoudhary@gmail.com>
2023-12-22 16:05:47 +0530

    Event update

--- data/Eventdata.js
+++ data/Eventdata.js
@@ -8,24 +8,35 @@ const eventData = {
       eventName: "Interaction with Divya Prakash Dubey & Ayush Keshri",
       eventDate: "05-04-2021",
       eventLocation: "Human Library Cafe",
-      eventDescription: "Fusce in est non purus bibendum efficitur. Sed eu libero a justo convallis iaculis eu et lectus.",
-      imageURL: 'evImage3.jpg'
+      eventDescription:
+        "Fusce in est non purus bibendum efficitur. Sed eu libero a justo convallis iaculis eu et lectus.",
+      imageURL: "evImage3.jpg",
     },
     {
       eventName: "Photography Workshop with Hari Mohan Kaushik",
       eventDate: "10-04-2021",
       eventLocation: "Human Library Cafe",
-      eventDescription: "Vestibulum tincidunt ante nec neque posuere, ut tempor tellus tincidunt. Vivamus sit amet felis eu arcu vehicula venenatis.",
-      imageURL: 'evImage2.jpg'
+      eventDescription:
+        "Vestibulum tincidunt ante nec neque posuere, ut tempor tellus tincidunt. Vivamus sit amet felis eu arcu vehicula venenatis.",
+      imageURL: "evImage2.jpg",
     },
     {
       eventName: "Interaction with Dr. Vinod Kumar Mall & Ayush Keshri",
       eventDate: "09-05-2021",
       eventLocation: "Human Library Cafe",
-      eventDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula libero at purus porta, in luctus sapien consectetur.",
-      imageURL: '/evImage1.jpg'
+      eventDescription:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula libero at purus porta, in luctus sapien consectetur.",
+      imageURL: "/evImage1.jpg",
     },
-  ]
+    {
+      eventName: "Calling all bookworms and wordsmiths!",
+      eventDate: "15-01-2024 to 17-01-2024",
+      eventLocation: "Varanasi, Human Library Cafe",
+      eventDescription:
+        "The HLC Literature Festival is returning with a captivating theme: [Festival Theme]. Join us for [Dates] as we celebrate the power of storytelling through vibrant panel discussions, author readings, and workshops. Get ready to be inspired, informed, and immersed in the world of literature! Don't miss out - register now.",
+      imageURL: "/evImage4.jpg",
+    },
+  ],
 };
 
 export default eventData;
